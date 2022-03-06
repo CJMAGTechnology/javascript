1bc4c515f96887e05baddffad9ccd3404f1d9631
CJMAGTechnology <jethro45@hotmail.com>
2022-03-05 18:50:06 -0600

    45

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -53,14 +53,10 @@ async function getQuotes() {
     loading()
     const apiUrl = 'https://type.fit/api/quotes'
     try{
-
         const response = await fetch(apiUrl)
         apiQuotes = await response.json()
         newQuote()
-
-
     }catch (error) {
-
         getQuotes()
 
     }
